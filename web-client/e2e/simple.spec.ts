0664eba46dafe4cddc3ef6c639d2c12ee33e238a
Saada Jugurtha <jugurtha.saada.etu@univ-lille.fr>
2025-01-03 12:03:15 +0100

    :construction_worker: Fix deploy.yml & docker-compose.yml

--- web-client/e2e/simple.spec.ts
+++
@@ -1,7 +0,0 @@
-// e2e/simple.spec.ts
-import { test, expect } from '@playwright/test';
-
-test('example test', async ({ page }) => {
-  await page.goto('https://example.com');
-  expect(await page.title()).toBe('Example Domain');
-});
